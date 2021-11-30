643fa0bd9242aa0a66a74656e2681364dc3763d5
Hoai Vi <vilth19411@st.uel.edu.vn>
2021-11-30 16:37:39 +0700

    Add my_rating_activity và các fragment rating

---
+++ app/src/main/java/com/example/merakibook/FiveStarRatingFragment.java
@@ -0,0 +1,28 @@
+package com.example.merakibook;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FiveStarRatingFragment extends Fragment {
+
+    public FiveStarRatingFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_five_star_rating, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/merakibook/MyRatingActivity.java
@@ -0,0 +1,89 @@
+package com.example.merakibook;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MyRatingActivity extends AppCompatActivity {
+
+    Button btnAllStar, btnFiveStar, btnFourStar, btnThreeStar, btnTwoStar, btnOneStar;
+    LinearLayout linearLayoutContainer;
+    Fragment fragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_rating);
+
+        linkView();
+        addEvent();
+    }
+
+    private void linkView() {
+        btnAllStar = findViewById(R.id.btnAllStar);
+        btnFiveStar = findViewById(R.id.btnFiveStar);
+        btnFourStar = findViewById(R.id.btnFourStar);
+        btnThreeStar = findViewById(R.id.btnThreeStar);
+        btnTwoStar = findViewById(R.id.btnTwoStar);
+        btnOneStar = findViewById(R.id.btnOneStar);
+
+        linearLayoutContainer = findViewById(R.id.linearLayoutContainer);
+    }
+
+    private void addEvent() {
+        btnAllStar.setOnClickListener(myClick);
+        btnFiveStar.setOnClickListener(myClick);
+        btnFourStar.setOnClickListener(myClick);
+        btnThreeStar.setOnClickListener(myClick);
+        btnTwoStar.setOnClickListener(myClick);
+        btnOneStar.setOnClickListener(myClick);
+    }
+
+    View.OnClickListener myClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            FragmentManager manager = getSupportFragmentManager();
+            FragmentTransaction transaction = manager.beginTransaction();
+            switch (view.getId()){
+                case R.id.btnAllStar:
+                    fragment = new AllStarRatingFragment();
+                    break;
+                case R.id.btnFiveStar:
+                    fragment = new FiveStarRatingFragment();
+                    break;
+                case R.id.btnFourStar:
+                    fragment = new FourStarRatingFragment();
+                    break;
+                case R.id.btnThreeStar:
+                    fragment = new ThreeStarRatingFragment();
+                    break;
+                case R.id.btnTwoStar:
+                    fragment = new TwoStarRatingFragment();
+                    break;
+                case R.id.btnOneStar:
+                    fragment = new OneStarRatingFragment();
+                    break;
+            }
+
+            transaction.replace(R.id.linearLayoutContainer,fragment);
+            transaction.commit();
+//            if(view.getId() == R.id.btnAllStar){
+//                fragment = new AllStarRatingFragment();
+//            }
+//
+//            if(view.getId() == R.id.btnFiveStar){
+//                fragment = new FiveStarRatingFragment();
+//            }
+//
+//            if(view.getId() == R.id.btnFourStar){
+//                fragment =
+//            }
+        }
+    };
+}
