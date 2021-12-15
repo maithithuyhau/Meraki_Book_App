b5cff177ce7f3a9d1e5087a064bf5f817203c2e5
Hoai Vi <vilth19411@st.uel.edu.vn>
2021-12-15 10:23:36 +0700

    Add interaction event between PaymentOption và QRCode

--- app/src/main/java/com/example/merakibook/PaymentOptionActivity.java
+++ app/src/main/java/com/example/merakibook/PaymentOptionActivity.java
@@ -2,13 +2,86 @@ package com.example.merakibook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class PaymentOptionActivity extends AppCompatActivity {
+
+    RadioGroup rdgPaymentOption;
+    RadioButton rdoCOD, rdoATM, rdoVisa;
+    Button btnAgree;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_option);
+
+        linkView();
+        addEvent();
     }
+
+    private void linkView() {
+        rdgPaymentOption = findViewById(R.id.rdgPaymentOption);
+        btnAgree = findViewById(R.id.btnAgree);
+        rdoCOD = findViewById(R.id.rdoCOD);
+        rdoATM = findViewById(R.id.rdoATM);
+        rdoVisa = findViewById(R.id.rdoVisa);
+    }
+
+    private void addEvent() {
+        btnAgree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int checkedOption = rdgPaymentOption.getCheckedRadioButtonId();
+                if (checkedOption == -1){
+                    Toast.makeText(PaymentOptionActivity.this, "Hãy chọn một phương thức thanh toán!", Toast.LENGTH_SHORT).show();
+                }else{
+                    selectedPayment(checkedOption);
+                }
+            }
+        });
+    }
+
+    private void selectedPayment(int checkedId){
+        switch (checkedId){
+            case R.id.rdoCOD:
+                QRcodeActivity qRcodeActivity = new QRcodeActivity();
+                qRcodeActivity.openDialog(PaymentOptionActivity.this);
+                break;
+            case R.id.rdoATM:
+                openQRActivityWithATM();
+                break;
+            case R.id.rdoVisa:
+                openQRActivityWithVisa();
+                break;
+        }
+    }
+
+    private void openQRActivityWithATM(){
+        String atm = "Sử dụng ứng dụng trên di động của ngân hàng (mobile banking) quét mã QR bên dưới để thanh toán.";
+        Intent intent = new Intent(PaymentOptionActivity.this, QRcodeActivity.class);
+        intent.putExtra("atm",atm);
+        PaymentOptionActivity.this.startActivity(intent);
+    }
+
+
+
+    private void openQRActivityWithVisa(){
+        String visa = "Sử dụng ứng dụng thẻ ghi nợ trên thiết bị di động quét mã QR bên dưới để thanh toán.";
+        Intent intent = new Intent(PaymentOptionActivity.this, QRcodeActivity.class);
+        intent.putExtra("visa",visa);
+        startActivity(intent);
+    }
+
+
 }
--- app/src/main/java/com/example/merakibook/QRcodeActivity.java
+++ app/src/main/java/com/example/merakibook/QRcodeActivity.java
@@ -3,17 +3,21 @@ package com.example.merakibook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class QRcodeActivity extends AppCompatActivity {
 
-    Button btnCheck, btnBackHome, btnDetailOrder;
+    Button btnCheck, btnBackHome;
+    TextView txtTittle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,35 +26,57 @@ public class QRcodeActivity extends AppCompatActivity {
 
         linkView();
         addEvent();
-
+//        getDataVisa();
+//        getDataATM();
     }
 
     private void linkView() {
         btnCheck = findViewById(R.id.btnCheck);
         btnBackHome = findViewById(R.id.btnBackHome);
+        txtTittle = findViewById(R.id.txtTittle);
     }
 
     private void addEvent() {
         btnCheck.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openDialog();
+                openDialog(QRcodeActivity.this);
             }
-
-
+        });
+        btnBackHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(QRcodeActivity.this,MainActivity.class);
+                QRcodeActivity.this.startActivity(intent);
+            }
         });
     }
-    public void openDialog(){
 
-        Dialog dialog = new Dialog(QRcodeActivity.this);
+    private void getDataVisa() {
+        Intent intent = getIntent();
+        String visa = intent.getStringExtra("visa");
+        txtTittle.setText(visa);
+    }
+
+    private void getDataATM() {
+        Intent intent = getIntent();
+        String atm = intent.getStringExtra("atm");
+        txtTittle.setText(atm);
+    }
+
+    public void openDialog(Context context){
+        Dialog dialog = new Dialog(context);
         dialog.setContentView(R.layout.popup_order_successful);
+
         Button btnDetailOrder = dialog.findViewById(R.id.btnDetailOrder);
         btnDetailOrder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(context,PurchaseOrderActivity.class);
+                context.startActivity(intent);
             }
         });
+
         dialog.getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         dialog.show();
