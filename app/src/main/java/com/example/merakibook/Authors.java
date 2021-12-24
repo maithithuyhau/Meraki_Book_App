20049a6331aa13e0c6075f226e84b5693c964258
maithithuyhau <haumtt19411@st.uel.edu.vn>
2021-12-24 19:38:11 +0700

    update Author and BookOfAuthor

--- app/src/main/java/com/example/merakibook/Authors.java
+++ app/src/main/java/com/example/merakibook/Authors.java
@@ -1,6 +1,7 @@
 package com.example.merakibook;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.ActivityOptions;
@@ -12,6 +13,8 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.adapter.BookAdapterHorizontal;
+import com.example.adapter.BookAdapterVertical;
 import com.example.model.Author;
 import com.example.model.AuthorItemClickListener;
 import com.example.model.Book;
@@ -27,7 +30,8 @@ public class Authors extends AppCompatActivity implements BookItemClickListener,
     private TextView txtTenTacGia1, txtTenTacGia2, txtNamSinh, txtNamMat, txtSoLuongTacPham, txtQueQuan, txtTomTat, txtXemTatCa;
     private CircleImageView imvAuthor;
     private RecyclerView rcvCacTacPham;
-
+    ArrayList<Book> myBook;
+    BookAdapterHorizontal bookAdapter;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +40,7 @@ public class Authors extends AppCompatActivity implements BookItemClickListener,
         linkViews();
         loadData();
         addEvent();
+        intiData();
     }
 
 
@@ -52,6 +57,19 @@ public class Authors extends AppCompatActivity implements BookItemClickListener,
         rcvCacTacPham=findViewById(R.id.rcvCacTacPham);
 
         imvAuthor=findViewById(R.id.imvAuthor);
+    }
+
+    private void intiData() {
+        myBook = new ArrayList<>();
+        myBook.add(new Book("Lão Hạc","Nam Cao","150","15,000","45,000","Nhà Xuất Bản Văn Học","2021-07-14 11:36:27","Bìa mềm","13 x 20.5 cm",R.string.sach_moi,R.drawable.laohacreal,R.string.sapiens));
+        myBook.add(new Book("Sống mòn","Nam Cao","170","25,000","83,000 ","Nhà Xuất Bản Văn Học","2021-01-12","Bìa mềm","15 x 24.5 cm",R.string.sach_moi,R.drawable.songmon,R.string.sinh_trac_van_tay));
+        myBook.add(new Book("Chí Phèo","Nam Cao","190","35,000","113,000 ","Nhà Xuất Bản Văn Học","2021-01-12","Bìa mềm","15 x 24.5 cm",R.string.sach_moi,R.drawable.chipheo,R.string.sinh_trac_van_tay));
+        myBook.add(new Book("Đời thừa","Nam Cao","150","25,000","65,000","Nhà Xuất Bản Văn Học","2021-07-14 11:36:27","Bìa mềm","13 x 20.5 cm",R.string.sach_moi,R.drawable.doithua,R.string.sapiens));
+        myBook.add(new Book("Đôi mắt","Nam Cao","170","25,000","50,000 ","Nhà Xuất Bản Văn Học","2021-01-12","Bìa mềm","15 x 24.5 cm",R.string.sach_moi,R.drawable.doimat,R.string.sinh_trac_van_tay));
+        myBook.add(new Book("Giăng sáng","Nam Cao","190","30,000","75,000 ","Nhà Xuất Bản Văn Học","2021-01-12","Bìa mềm","15 x 24.5 cm",R.string.sach_moi,R.drawable.giangsang,R.string.sinh_trac_van_tay));
+        bookAdapter= new BookAdapterHorizontal(this,myBook,this);
+        rcvCacTacPham.setAdapter(bookAdapter);
+        rcvCacTacPham.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
     }
 
     private void addEvent() {
@@ -87,7 +105,23 @@ public class Authors extends AppCompatActivity implements BookItemClickListener,
 
     @Override
     public void onBookClick(Book book, ImageView bookImageView) {
+        Intent intent=new Intent(this, DetailBookActivity.class);
+        intent.putExtra(Constant.BOOK_NAME,book.getBookName());
+        intent.putExtra(Constant.BOOK_SUMMARY,book.getBookSummary());
+        intent.putExtra(Constant.BOOK_IMAGE,book.getBookImage());
+        intent.putExtra(Constant.BOOK_AUTHOR,book.getBookAuthor());
+        intent.putExtra(Constant.BOOK_PAGE,book.getBookPage());
+        intent.putExtra(Constant.BOOK_E_PRICE,book.getEbookPrice());
+        intent.putExtra(Constant.BOOK_PRICE,book.getBookPrice());
+        intent.putExtra(Constant.BOOK_PUBLISHER,book.getBookPublisher());
+        intent.putExtra(Constant.BOOK_DATETIME,book.getDateTime());
+        intent.putExtra(Constant.BOOK_LOAI_BIA,book.getLoaiBia());
+        intent.putExtra(Constant.BOOK_SIZE,book.getBookSize());
 
+//        startActivity(intent);
+        //Animation
+        ActivityOptions options=ActivityOptions.makeSceneTransitionAnimation(Authors.this, bookImageView,"sharedName");
+        startActivity(intent,options.toBundle());
     }
 
     @Override
--- app/src/main/java/com/example/merakibook/BooksOfAuthorActivity.java
+++ app/src/main/java/com/example/merakibook/BooksOfAuthorActivity.java
@@ -1,7 +1,9 @@
 package com.example.merakibook;
 
 //import android.content.Context;
+import android.app.ActivityOptions;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,26 +15,20 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.adapter.BookAdapterVertical;
+import com.example.adapter.BookAdapterVerticalFragment;
 import com.example.model.Book;
 import com.example.model.BookItemClickListener;
+import com.example.utils.Constant;
 
 import java.util.ArrayList;
 
 public class BooksOfAuthorActivity extends AppCompatActivity implements BookItemClickListener {
 
-    View view;
     RecyclerView rcvBooksOfAuthor;
     ArrayList<Book> myBook;
     BookAdapterVertical bookAdapterVertical;
     
-    public void BooksOfAuthor(){
-        
-    }
 
-    @Override
-    public void onBookClick(Book book, ImageView bookImageView) {
-
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,11 +39,10 @@ public class BooksOfAuthorActivity extends AppCompatActivity implements BookItem
         initData();
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return null;
-    }
 
+    private void linkView() {
+        rcvBooksOfAuthor = findViewById(R.id.rcvBooksOfAuthor);
+    }
 
     private void initData() {
         myBook = new ArrayList<>();
@@ -57,11 +52,37 @@ public class BooksOfAuthorActivity extends AppCompatActivity implements BookItem
         myBook.add(new Book("Đời thừa","Nam Cao","150","25,000","65,000","Nhà Xuất Bản Văn Học","2021-07-14 11:36:27","Bìa mềm","13 x 20.5 cm",R.string.sach_moi,R.drawable.doithua,R.string.sapiens));
         myBook.add(new Book("Đôi mắt","Nam Cao","170","25,000","50,000 ","Nhà Xuất Bản Văn Học","2021-01-12","Bìa mềm","15 x 24.5 cm",R.string.sach_moi,R.drawable.doimat,R.string.sinh_trac_van_tay));
         myBook.add(new Book("Giăng sáng","Nam Cao","190","30,000","75,000 ","Nhà Xuất Bản Văn Học","2021-01-12","Bìa mềm","15 x 24.5 cm",R.string.sach_moi,R.drawable.giangsang,R.string.sinh_trac_van_tay));
-
+        bookAdapterVertical = new BookAdapterVertical(this,myBook,this);
+        rcvBooksOfAuthor.setLayoutManager(new LinearLayoutManager(this));
+        rcvBooksOfAuthor.setAdapter(bookAdapterVertical);
     }
 
-    private void linkView() {
-        rcvBooksOfAuthor = view.findViewById(R.id.rcvBooksOfAuthor);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return null;
+    }
+
+    @Override
+    public void onBookClick(Book book, ImageView bookImageView) {
+        Intent intent=new Intent(this, DetailBookActivity.class);
+        intent.putExtra(Constant.BOOK_NAME,book.getBookName());
+        intent.putExtra(Constant.BOOK_SUMMARY,book.getBookSummary());
+        intent.putExtra(Constant.BOOK_IMAGE,book.getBookImage());
+        intent.putExtra(Constant.BOOK_AUTHOR,book.getBookAuthor());
+        intent.putExtra(Constant.BOOK_PAGE,book.getBookPage());
+        intent.putExtra(Constant.BOOK_E_PRICE,book.getEbookPrice());
+        intent.putExtra(Constant.BOOK_PRICE,book.getBookPrice());
+        intent.putExtra(Constant.BOOK_PUBLISHER,book.getBookPublisher());
+        intent.putExtra(Constant.BOOK_DATETIME,book.getDateTime());
+        intent.putExtra(Constant.BOOK_LOAI_BIA,book.getLoaiBia());
+        intent.putExtra(Constant.BOOK_SIZE,book.getBookSize());
+
+
+//        startActivity(intent);
+
+        //Animation
+        ActivityOptions options=ActivityOptions.makeSceneTransitionAnimation(BooksOfAuthorActivity.this, bookImageView,"sharedName");
+        startActivity(intent,options.toBundle());
     }
 
 }
